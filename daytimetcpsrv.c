cc4559f7b668ebb659dc9d2d53d712b7caba71f8
jckmilk <a771455081@gmail.com>
2018-12-17 19:08:02 +0800

    client and server

---
+++ daytimetcpsrv.c
@@ -0,0 +1,26 @@
+#include"unp.h"
+#include<time.h>
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    struct sockaddr_in servaddr;
+    char buff[MAXLINE];
+    time_t ticks;
+    listenfd=Socket(AF_INET,SOCK_STREAM,0);
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=hton1(INADDR_ANY);
+    servaddr.sin_port=htons(13);
+    bind(listenfd, (SA*)&servaddr, sizeof(servaddr));
+    listen(listenfd, LISTENQ);
+    for( ;)
+    {
+        connfd=Accept(listenfd, (SA*)NULL, NULL);
+        ticks=time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+        Close(connfd);
+        
+    }
+}
